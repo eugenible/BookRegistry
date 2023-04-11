b6f9ae782d8b80a1c1dbbdeaa7ba3bb1918352cb
eugenible <ztretyakov@gmail.com>
2023-04-11 15:44:39 +0700

    Changed welcoming page

--- src/main/java/ru/eugenible/registry/config/SpringConfig.java
+++ src/main/java/ru/eugenible/registry/config/SpringConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -51,6 +53,12 @@ public class SpringConfig implements WebMvcConfigurer {
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
         return templateEngine;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/WEB-INF/views/welcome.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
 
     // Тут подключается поддержка UTF-8
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Welcome to library system!</h2>
+
+<a href="http://localhost:8080/people">List of readers</a>
+<hr/>
+<a href="http://localhost:8080/people">List of books</a>
+
 </body>
 </html>
